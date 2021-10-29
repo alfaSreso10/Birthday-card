5a5e88c260d310eabbe5b94cd09b7cb96255ec66
alfaSreso10 <77802304+alfaSreso10@users.noreply.github.com>
2021-10-29 23:35:04 +0530

    Add files via upload

---
+++ count.js
@@ -0,0 +1,70 @@
+document.getElementById("h").style.backgroundColor = "#d63384";
+
+/*for (let i = 11000; i >= 1000; i-=1000) 
+{
+    setTimeout(function () {
+        if(i===11000)
+        {
+            document.getElementById("h").innerHTML = "HAPPY BIRTHDAY....";
+            document.getElementById("cake").innerHTML=" <img src='cake.gif' width='350px' height='300px'>"
+        }
+        else
+        document.getElementById("h").innerHTML =((11000-i)/1000);
+
+        document.getElementById("h").style.color = "yellow";
+    }, i)
+}*/
+setTimeout(function(){
+    document.getElementById("h").innerHTML ="10"
+    document.getElementById("h").style.color = "yellow";
+},1000);
+
+setTimeout(function(){
+    document.getElementById("h").innerHTML ="9"
+    document.getElementById("h").style.color = "yellow";
+},2000);
+
+setTimeout(function(){
+    document.getElementById("h").innerHTML ="8"
+    document.getElementById("h").style.color = "yellow";
+},3000);
+
+setTimeout(function(){
+    document.getElementById("h").innerHTML ="7"
+    document.getElementById("h").style.color = "yellow";
+},4000);
+
+setTimeout(function(){
+    document.getElementById("h").innerHTML ="6"
+    document.getElementById("h").style.color = "yellow";
+},5000);
+
+setTimeout(function(){
+    document.getElementById("h").innerHTML ="5"
+    document.getElementById("h").style.color = "yellow";
+},6000);
+
+setTimeout(function(){
+    document.getElementById("h").innerHTML ="4"
+    document.getElementById("h").style.color = "yellow";
+},7000);
+
+setTimeout(function(){
+    document.getElementById("h").innerHTML ="3"
+    document.getElementById("h").style.color = "yellow";
+},8000);
+
+setTimeout(function(){
+    document.getElementById("h").innerHTML ="2"
+    document.getElementById("h").style.color = "yellow";
+},9000);
+
+setTimeout(function(){
+    document.getElementById("h").innerHTML ="1"
+    document.getElementById("h").style.color = "yellow";
+},10000);
+
+setTimeout(function(){
+    document.getElementById("h").innerHTML = "HAPPY BIRTHDAY....";
+    document.getElementById("cake").innerHTML=" <img src='cake.gif' width='350px' height='300px'>"
+},11000);
